79d808ff1bb0ff6b68cf670263a887481deaa922
Getachew Tebikew <getachewtbkw@gmail.com>
2021-09-14 11:35:03 +0300

    hide unnecessary footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,31 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'go-fund-me';
+  no_footer_pages = ['/account', '/create', '/admin', '/notification', '/sign'];
 
-  // // for google analytics
-  // constructor(private router: Router){
-  //   const navEndEvents=router.events.pipe(
-  //     filter(event=> event instanceof NavigationEnd)
-  //   );
-  //   navEndEvents.subscribe((event)=>{
-  //     console.log((event as NavigationEnd).url);
-  //   });
-  // }
+  hide_footer = false;
 
+  constructor(private router: Router) {
+    const navEndEvents = router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    );
+    navEndEvents.subscribe((event) => {
+      this.hide_footer=false;
+      let url = (event as NavigationEnd).url;
+      this.no_footer_pages.forEach(_page=>{
+        if (url.startsWith(_page)) {
+          this.hide_footer=true;
+          return;
+        }
+      })
+    });
+  }
 }
