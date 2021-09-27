24bb21ba1237b59079dd52d471a05051a39e0a8c
Getachew Tebikew <getachewtbkw@gmail.com>
2021-09-28 00:20:54 +0300

    add withdrawal status for payment filtering

--- src/app/admin/components/payments/payments.component.ts
+++ src/app/admin/components/payments/payments.component.ts
@@ -50,10 +50,10 @@ export class PaymentsComponent implements OnInit {
   fundraiserPage?: FundraiserPage;
   allFundraisers: Fundraiser[] = [];
   fundsWithFullData: Fundraiser[] = [];
-  exchangeRate: number=1;
-  
+  exchangeRate: number = 1;
+
   exchangeRateSubscription?: Subscription;
-  
+
   constructor(
     private currencyConverterService: CurrencyConverterService,
     private cdr: ChangeDetectorRef,
@@ -63,13 +63,12 @@ export class PaymentsComponent implements OnInit {
 
   async ngOnInit() {
     this.pageTitle.setTitle('Admin | payments to be exported');
-    this.loading=true;
+    this.loading = true;
     await this.getAllFundraisers();
     console.log(this.allFundraisers);
     await this.getFilteredFundraiserDetail();
     console.log(this.fundsWithFullData);
     await this.activateTable();
-    this.loading=false;
   }
 
   // // listen to event date range change from the date range selector
@@ -101,7 +100,6 @@ export class PaymentsComponent implements OnInit {
   // }
 
   // get all available fundraisers
- 
 
   // // filter fundraisers that have accepted withdrawal status and donations in the given date range
   // filterFundraisers(startDate: Date, endDate: Date) {
@@ -137,7 +135,6 @@ export class PaymentsComponent implements OnInit {
   //   return total;
   // }
 
-
   async getAllFundraisers() {
     do {
       await this.getFundraisers();
@@ -151,7 +148,6 @@ export class PaymentsComponent implements OnInit {
         this.fundraiserPage = fundraiserPage;
         console.log(fundraiserPage.fundraisers);
         this.allFundraisers.push(...fundraiserPage.fundraisers);
-        this.loading = false;
         this.currentPage++;
       },
       (error: HttpErrorResponse) => {
@@ -161,30 +157,36 @@ export class PaymentsComponent implements OnInit {
     );
   }
 
-  // get fundraiser details for those having donations and totalRaised-totalWithdrawn>0
+  // get fundraiser details 
+  // for those having donations 
+  // and totalRaised-totalWithdrawn>0, 
+  // and its withdrawal request is accepted
   async getFilteredFundraiserDetail() {
-    return new Promise(async resolve=>{
-      await this.allFundraisers.forEach(async (fund,index) => {
-      await this.fundraiserService.getFundraiserAsync(fund._id!).then(
-        (fundraiser) => {
-          if (
-            fundraiser.donations?.length! > 0 &&
-            (this.getTotalRaised(fundraiser)- this.getTotalWithdrawn(fundraiser) > 0)
-          ) {
-            this.fundsWithFullData.push(fundraiser);
+    return new Promise(async (resolve) => {
+      await this.allFundraisers.forEach(async (fund, index) => {
+        await this.fundraiserService.getFundraiserAsync(fund._id!).then(
+          (fundraiser) => {
+            if (
+              fundraiser.donations?.length! > 0 &&
+              this.getTotalRaised(fundraiser) -
+                this.getTotalWithdrawn(fundraiser) >
+                0 &&
+              fundraiser.withdraw?.status?.toLowerCase() === 'accepted'
+            ) {
+              this.fundsWithFullData.push(fundraiser);
+            }
+          },
+          (error: HttpErrorResponse) => {
+            console.log(error.error);
           }
-        },
-        (error: HttpErrorResponse) => {
-          console.log(error.error);
+        );
+        if (index === this.allFundraisers.length - 1) {
+          resolve(true);
+          this.loading = false;
         }
-      );
-      if (index===this.allFundraisers.length-1) {
-        resolve(true);
-      }
+      });
     });
-  });
-}
-
+  }
 
   // add fundraiser data to payments array
   addToPayments(fundraiser: Fundraiser) {
@@ -193,7 +195,8 @@ export class PaymentsComponent implements OnInit {
       : fundraiser.organizer!;
     let withdrawal = fundraiser.withdraw;
     let payment: Payment = {
-      amount: this.getTotalRaised(fundraiser) - this.getTotalWithdrawn(fundraiser),
+      amount:
+        this.getTotalRaised(fundraiser) - this.getTotalWithdrawn(fundraiser),
       firstName: user.firstName,
       lastName: user.lastName,
       fundraiserId: fundraiser._id!,
@@ -209,12 +212,12 @@ export class PaymentsComponent implements OnInit {
       this.addToPayments(fund);
     });
     console.log(this.payments);
-    
+
     // assign the payments array to our data source
     this.dataSource.data = this.payments;
     this.cdr.detectChanges();
     this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    // this.dataSource.sort = this.sort;
   }
 
   // returns the total withdrawn amount of a fundraiser
@@ -229,8 +232,8 @@ export class PaymentsComponent implements OnInit {
     return total;
   }
 
-   // returns the percentage of the total raised by this fundraiser to its goal
-   getPercentage(fund: Fundraiser): number {
+  // returns the percentage of the total raised by this fundraiser to its goal
+  getPercentage(fund: Fundraiser): number {
     return (
       ((fund.totalRaised?.birr ??
         0 + (fund.totalRaised?.dollar ?? 0 * this.exchangeRate)) /
@@ -262,7 +265,5 @@ export class PaymentsComponent implements OnInit {
 
   ngOnDestroy(): void {
     this.exchangeRateSubscription?.unsubscribe();
-    
   }
-
 }
