0a56fea32b0a4365ad30187667bb8d864aa32a7e
Getachew Tebikew <getachewtbkw@gmail.com>
2021-09-28 23:35:23 +0300

    currency conversion

--- src/app/admin/components/statistics/statistics.component.ts
+++ src/app/admin/components/statistics/statistics.component.ts
@@ -10,16 +10,17 @@ import { FundraiserService } from 'src/app/services/fundraiser/fundraiser.servic
 import { AdminDonationsService } from '../../services/admin-donations/admin-donations.service';
 import { AdminUsersService } from '../../services/admin-users/admin-users.service';
 import { Title } from '@angular/platform-browser';
+import { CurrencyConverterService } from './../../../services/currency-converter/currency-converter.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'admin-statistics',
   templateUrl: './statistics.component.html',
-  styleUrls: ['./statistics.component.scss']
+  styleUrls: ['./statistics.component.scss'],
 })
 export class StatisticsComponent implements OnInit {
-  coinIcon= faCoins;
-  donateIcon= faDonate;
-
+  coinIcon = faCoins;
+  donateIcon = faDonate;
 
   fundraiserPage?: FundraiserPage;
 
@@ -42,21 +43,28 @@ export class StatisticsComponent implements OnInit {
 
   dateChosen: Date = new Date('0');
   endDate: Date = new Date();
+
+  // currency
+  exchangeRateSubscription?: Subscription;
+  exchangeRate: number = 1;
   constructor(
     private fundraiserService: FundraiserService,
     private usersService: AdminUsersService,
-    private adminDonationService: AdminDonationsService
-   , private title: Title,
-    ) { }
+    private adminDonationService: AdminDonationsService,
+    private title: Title,
+    private currencyService: CurrencyConverterService
+  ) {}
 
   async ngOnInit() {
     this.title.setTitle('Admin | statistics');
     this.loading = true;
+    this.getExchangeRate();
+
     await this.getAllFundraisers();
     this.getFundraisersByDate(this.dateChosen, this.endDate);
 
     // console.log(this.allFundraisers);
-    
+
     await this.getUsers();
     this.getUsersByDate(this.dateChosen, this.endDate);
 
@@ -66,13 +74,32 @@ export class StatisticsComponent implements OnInit {
     this.loading = false;
   }
 
+  // get the current currency exchange rate
+  getExchangeRate() {
+    this.exchangeRateSubscription = this.currencyService
+      .getExchangeRate()
+      .subscribe(
+        (rate) => {
+          if (rate) {
+            this.exchangeRate = rate.USD_ETB;
+          }
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error.error);
+        }
+      );
+  }
+
   // when a user select a date range
-  async dateRangeChange(dateRangeStart: HTMLInputElement, dateRangeEnd: HTMLInputElement) {
+  async dateRangeChange(
+    dateRangeStart: HTMLInputElement,
+    dateRangeEnd: HTMLInputElement
+  ) {
     this.totalRaised = 0;
-    this.totalTip=0;
+    this.totalTip = 0;
     this.loading = true;
-    let startDate = new Date(dateRangeStart.value)
-    let endDate = new Date(dateRangeEnd.value)
+    let startDate = new Date(dateRangeStart.value);
+    let endDate = new Date(dateRangeEnd.value);
     await this.getFundraisersByDate(startDate, endDate);
     await this.getUsersByDate(startDate, endDate);
     await this.getFilteredDonationsByDate(startDate, endDate);
@@ -85,44 +112,43 @@ export class StatisticsComponent implements OnInit {
       await this.getFundraisers();
     } while (this.currentPage < this.fundraiserPage?.totalPages!);
     // console.log(this.allFundraisers);
-    
   }
 
   //get fundraisers of a page
   async getFundraisers() {
     this.loading = true;
-    await this.fundraiserService
-      .getAdminFundraisers(this.currentPage)
-      .then(
-        (fundraiserPage: FundraiserPage) => {
-          this.fundraiserPage = fundraiserPage;
-          console.log(fundraiserPage.fundraisers);
-          
-          this.allFundraisers = [
-            ...this.allFundraisers,
-            ...fundraiserPage.fundraisers,
-          ];
-          this.loading = false;
-          this.currentPage++;
-        },
+    await this.fundraiserService.getAdminFundraisers(this.currentPage).then(
+      (fundraiserPage: FundraiserPage) => {
+        this.fundraiserPage = fundraiserPage;
+        console.log(fundraiserPage.fundraisers);
 
-        (error: HttpErrorResponse) => {
-          this.loading = false;
-          console.log(error.error);
-          this.errorMessage = 'Unable to load fundraisers';
-        }
-      );
+        this.allFundraisers = [
+          ...this.allFundraisers,
+          ...fundraiserPage.fundraisers,
+        ];
+        this.loading = false;
+        this.currentPage++;
+      },
+
+      (error: HttpErrorResponse) => {
+        this.loading = false;
+        console.log(error.error);
+        this.errorMessage = 'Unable to load fundraisers';
+      }
+    );
   }
 
   // filter fundraisers by date
   async getFundraisersByDate(startDate: Date, endDate: Date) {
-    this.filteredFundraisers = await this.allFundraisers.filter((fund: Fundraiser) => {
-      let creationDate=  new Date(fund.dateCreated!);
-      return startDate <= creationDate && creationDate <= endDate;
-    });
+    this.filteredFundraisers = await this.allFundraisers.filter(
+      (fund: Fundraiser) => {
+        let creationDate = new Date(fund.dateCreated!);
+        return startDate <= creationDate && creationDate <= endDate;
+      }
+    );
   }
 
-  // get #fundraisers 
+  // get #fundraisers
   getNumberOfFundraisersCreated(): number {
     return this.filteredFundraisers.length;
   }
@@ -130,50 +156,59 @@ export class StatisticsComponent implements OnInit {
   // get all users
   async getUsers() {
     await this.usersService.getAllUsers().then(
-      (users: User[]) => this.allUsers = users,
+      (users: User[]) => (this.allUsers = users),
       (error: HttpErrorResponse) => {
         console.log(error);
       }
-    )
+    );
   }
 
-  // get users by date 
+  // get users by date
   async getUsersByDate(startDate: Date, endDate: Date) {
-    this.filteredUsers = await this.allUsers.filter(user => {
+    this.filteredUsers = await this.allUsers.filter((user) => {
       let signupDate = new Date(user.date!);
-     return (startDate <= signupDate) && (signupDate <= endDate);
+      return startDate <= signupDate && signupDate <= endDate;
     });
   }
 
   //get all donations
   async getAllDonations() {
-    await this.adminDonationService.getAllDonations().then(
-      (donations) => this.allDonations = donations
-    );
+    await this.adminDonationService
+      .getAllDonations()
+      .then((donations) => (this.allDonations = donations));
   }
 
   //get filtered donations by date
   async getFilteredDonationsByDate(startDate: Date, endDate: Date) {
-    this.filteredDonations = await this.allDonations.filter(
-      (donation) => {
-        let donationDate = new Date(donation.date!);
-        return (startDate <= donationDate && donationDate <= endDate) 
-      });
-      await this.getTotalRaised();
+    this.filteredDonations = await this.allDonations.filter((donation) => {
+      let donationDate = new Date(donation.date!);
+      return startDate <= donationDate && donationDate <= endDate;
+    });
+    await this.getTotalRaised();
+    await this.getTotalTip();
   }
 
   // get total raised
   async getTotalRaised() {
-    await this.filteredDonations.forEach(
-      (donation) => {
-        this.totalRaised += donation.amount;
-        if (donation.tip) {
-          this.totalTip += donation.tip;
-        }
-        console.log(this.totalTip);
-      }
-    );
+    await this.filteredDonations.forEach((donation) => {
+      this.totalRaised +=
+        donation.paymentMethod?.toLowerCase() === 'paypal'
+          ? donation.amount * this.exchangeRate
+          : donation.amount;
+    });
   }
 
+  // get total tip
+  async getTotalTip() {
+    await this.filteredDonations.forEach((donation) => {
+      if (donation.tip) {
+        this.totalTip +=
+          donation.paymentMethod?.toLowerCase() === 'paypal'
+            ? donation.tip * this.exchangeRate
+            : donation.tip;
+      }
+    });
+    console.log(this.totalTip);
+    
+  }
 }
-
