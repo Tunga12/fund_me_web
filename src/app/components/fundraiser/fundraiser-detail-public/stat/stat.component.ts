09cacc2cb6fe4857b2a064327ff7f12b09d9036b
Getachew Tebikew <getachewtbkw@gmail.com>
2021-10-01 16:10:37 +0300

    currency conversion

--- src/app/components/fundraiser/fundraiser-detail-public/stat/stat.component.ts
+++ src/app/components/fundraiser/fundraiser-detail-public/stat/stat.component.ts
@@ -1,5 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { Fundraiser } from 'src/app/models/fundraiser.model';
+import { Subscription } from 'rxjs';
+import { CurrencyConverterService } from 'src/app/services/currency-converter/currency-converter.service';
 
 @Component({
   selector: 'app-stat',
@@ -9,18 +12,37 @@ import { Fundraiser } from 'src/app/models/fundraiser.model';
 export class StatComponent implements OnInit {
   @Input() fundraiser!: Fundraiser;
   percentage = 0;
-  @Input() exchangeRate!: number;
+  exchangeRate: number = 1;
   totalRaised: number = 0;
+  currencySub?: Subscription;
+
+  constructor(private currencyConvServ: CurrencyConverterService) {}
 
   ngOnInit(): void {
-    this.getTotalRaisedInBirr();
-    this.getPercentage();
+    this.getConversionRate();
   }
   //  total money raised by this fundraiser
   getTotalRaisedInBirr() {
     this.totalRaised =
       this.fundraiser.totalRaised?.birr! +
       this.fundraiser.totalRaised?.dollar! * this.exchangeRate;
+  }
+
+  // get currency conversion rate
+  getConversionRate() {
+    this.currencySub = this.currencyConvServ.getExchangeRate().subscribe(
+      (rate) => {
+        if (rate) {
+          this.exchangeRate = rate.USD_ETB;
+          console.log(rate);
+        }
+        this.getTotalRaisedInBirr();
+        this.getPercentage();
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.error);
+      }
+    );
   }
 
   // percentage of money raised to fundraiser goal amount
